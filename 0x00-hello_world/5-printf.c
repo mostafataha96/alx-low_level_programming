e71cb1d70a5be91502fbccabd9f512b28df7fcfb
Mostafa Taha <mostafa.taha.mt11@gmail.com>
2023-06-15 17:00:14 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - A program that print a line using the printf function
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
