070037100b8e311e74acf1f33d66778d53be835e
Mostafa Taha <mostafa.taha.mt11@gmail.com>
2023-06-20 08:41:57 -0700

    C nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
  * main - Entry point
  * Description: Prints the alphabet lower case
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	int i = 97;
 
-	while (i < 122)
+	while (i <= 122)
 	{
 		_putchar(i);
 		i++;
