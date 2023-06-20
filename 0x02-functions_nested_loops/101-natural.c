e25af3bf28d7b9cf71c54724accc6f9301c13431
Mostafa Taha <mostafa.taha.mt11@gmail.com>
2023-06-20 16:20:34 -0700

    C nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: computes sum of all
+ * the multiples of 3 or 5
+ * below 1024 (excluded)
+ * Return: Always 0 (SuccesS)
+ */
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0)
+				sum += sum;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
