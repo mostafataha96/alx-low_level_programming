6b476ce6a34bbfc87805ad59089fa49f5334f76a
Mostafa Taha <mostafa.taha.mt11@gmail.com>
2023-06-20 08:55:12 -0700

    C nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: Prints the alphabet lower case
+ * Description: print from a to z
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
