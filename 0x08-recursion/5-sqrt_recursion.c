a48acf7cf4f2ce751bfb439a78cbbcedbc7f33eb
Mostafa Taha <mostafa.taha.mt11@gmail.com>
2023-07-05 07:01:36 -0700

    C recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * @val: square root
+ * return: int
+ */
+int square(int n, int val);
+int _sqrt_recursion(innt n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - find square root
+ * @n: int to find sqaure root
+ * @val: square root
+ * return: int
+ */
+
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+}
