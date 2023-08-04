f90407a85c9abcdcab611ac965e12811d68b01b0
mostafataha96 <mostafa.taha.mt11@gmail.com>
2023-08-04 11:22:46 -0700

    Bit Manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,7 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, n = 0;
+	int i, count = 0;
 	unsigned long int current;
 
 	for (i = 63; i >= 0; i--)
@@ -16,11 +16,11 @@ void print_binary(unsigned long int n)
 		if (current & 1)
 		{
 			_putchar('1');
-			n++;
+			count++;
 		}
-		else if (n)
+		else if (count)
 			_putchar('0');
 	}
-	if (!n)
+	if (!count)
 		_putchar('0');
 }
